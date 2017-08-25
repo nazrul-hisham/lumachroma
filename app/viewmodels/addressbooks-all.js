b7599035235424df162e8ac9167e175078061d58
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-08-25 23:15:59 +0800

    implement pagination on addressbook all

--- app/viewmodels/addressbooks-all.js
+++ app/viewmodels/addressbooks-all.js
@@ -1,23 +1,81 @@
 ﻿define(['knockout'], function (ko) {
-    var list = ko.observableArray([]),
+    var url = "https://lumachroma.azurewebsites.net";
+    var url = "http://localhost:50521";
+    var isBusy = ko.observable(false),
+        list = ko.observableArray([]),
+        pagerSelf = ko.observable(null),
+        pagerNext = ko.observable(null),
+        pagerPrev = ko.observable(null),
+        pagerPage = ko.observable(1),
+        pagerSize = ko.observable(20),
+        pagerAvailSize = ko.observableArray([10, 20, 30, 50]),
     activate = function () {
-        var url = "https://lumachroma.azurewebsites.net";
+        pagerPage(1);
+        loadPage(`${url}/api/address-books?size=${pagerSize()}`);
+    },
+    attached = function () {
+        $('#page-pager-size').change(function () {
+            activate();
+        });
+    },
+    loadPage = function (url) {
+        pagerSelf(null);
+        pagerNext(null);
+        pagerPrev(null);
+        isBusy(true);
         $.ajax({
-            url: `${url}/api/address-books`,
+            url: url,
             method: "GET"
         }).done(function (result) {
             console.log(result);
             list(result._results);
+            $.each(result._links, function (index, item) {
+                if (item.rel == "self") pagerSelf(item);
+                if (item.rel == "next") pagerNext(item);
+                if (item.rel == "prev") pagerPrev(item);
+            });
+            isBusy(false);
+        }).fail(function (e) {
+            console.log(e.status);
+            console.log(e.responseJSON.status);
+            isBusy(false);
         });
     },
-    attached = function () {
-
+    pagerNextPage = function () {
+        console.log(pagerNext());
+        if (pagerNext()) {
+            pagerPage(pagerPage() + 1);
+            loadPage(pagerNext().href);
+        }
+    },
+    pagerPrevPage = function () {
+        console.log(pagerPrev());
+        if (pagerPrev()) {
+            pagerPage(pagerPage() - 1);
+            loadPage(pagerPrev().href);
+        }
+    },
+    pagerSelfPage = function () {
+        console.log(pagerSelf());
+        if (pagerSelf()) {
+            pagerPage(1);
+            loadPage(pagerSelf().href);
+        }
     };
 
     return {
+        isBusy: isBusy,
         list: list,
+        pagerNextPage: pagerNextPage,
+        pagerPrevPage: pagerPrevPage,
+        pagerSelfPage: pagerSelfPage,
+        pagerNext: pagerNext,
+        pagerPrev: pagerPrev,
+        pagerSelf: pagerSelf,
+        pagerSize: pagerSize,
+        pagerPage: pagerPage,
+        pagerAvailSize: pagerAvailSize,
         activate: activate,
         attached: attached
     };
 });
-
